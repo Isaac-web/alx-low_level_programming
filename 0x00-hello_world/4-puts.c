908df2c1394e2bb42276df1ba248037e298295e1
Isaac-web <kanytakiy@gmail.com>
2022-04-08 06:02:24 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * main - checks the code
  *
- * Return - always returns 0
- * */
+ * Return - always returns 0.
+ */
 int main(){
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
