75bc68a469e53b748d3de409a906934bb2684f18
Isaac-web <kanytakiy@gmail.com>
2022-04-15 15:15:40 +0000

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ *
+ *
+ * _isdigit - detects a number
+ *
+ *
+ * return - returns 0 for false and 1 for true
+ *
+ *
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <)
+}
