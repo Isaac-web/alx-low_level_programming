fae32c9c9915659a4d1b9b9cd1f2ac84e7259d1d
Isaac-web <kanytakiy@gmail.com>
2022-04-15 14:31:46 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/**
+ *
+ *_isupper - determines if the input is uppercase or not
+ *
+ *
+ *Return - returns 0 or 1 depending on the input
+ *
+ */
+int _isupper(int c)
+{
+	for(int i = 65; i < 90; i++)
+	{
+		if(i == c) return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+This session is about more nested loops and header files.
+Let
+Us
+See
+that the projects hold for us
