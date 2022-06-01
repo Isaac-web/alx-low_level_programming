5f0bbfc65b3745905267072298878f3e15de40e5
Isaac-web <kanytakiy@gmail.com>
2022-06-01 23:18:07 +0000

    Bit manipulation in c.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	return (0);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+int _putchar(char ch);
+unsigned int binary_to_uint(const char *b);
+#endif
