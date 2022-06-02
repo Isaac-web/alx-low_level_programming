9bed4dbfa7bc78224cd628a8021cc02205d172e3
Isaac-web <kanytakiy@gmail.com>
2022-06-02 01:09:06 +0000

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,19 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * power_two - returns two to the power a number
- * @pow - exponent to be raise
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  * Return: the converted number
  */
-
-
-int power_two(int pow)
-{	
-	if(pow == 0) 
-		return 1;
-
-	return 2 << pow-1;
-}
-
 unsigned int binary_to_uint(const char *b)
 {
-	int count = 0;
 	int total = 0;
-	int int_value = atoi(b);
-	
-	while(int_value > 0)
-	{
-		int value = int_value%10;
-		total += value * power_two(count);
 
-		int_value /= 10;
-		count++;
-	}
+	if(!b || b > '9' || b < 0)
+		return 0;
+
+	for(int i = 0; b[i]; i++)
+		total = 2 * total + (b[i] - '0');
 
 	return (total);
 }
