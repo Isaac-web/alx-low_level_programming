5ed45cd5243935bb9b208f1fdb1744e7f5339d94
Isaac-web <kanytakiy@gmail.com>
2022-06-02 01:26:21 +0000

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
@@ -7,12 +5,13 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	int total = 0;
-
 	if(!b)
 		return (0);
-
-	for(int i = 0; b[i]; i++)
+	
+	
+	for(i = 0; b[i]; i++)
 	{	
 		if(b[i] < '0' || b[0] > '9')
 			return (0);
