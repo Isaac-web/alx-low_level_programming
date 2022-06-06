2dfc4a54631d91b51b05b25bb89dba21a22662c1
Isaac-web <kanytakiy@gmail.com>
2022-06-06 16:58:05 +0000

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,6 +6,13 @@
 #include "main.h"
 
 
+/**
+ * append_text_to_file - adds text to a file
+ * @filename: name of the file
+ * @text_content: input to the file
+ * Returns: 1 or -1
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, write_count;
