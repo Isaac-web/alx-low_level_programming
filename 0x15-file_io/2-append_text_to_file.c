54c1743f35c78f91cc388ec6c2ed19407fe11d95
Isaac-web <kanytakiy@gmail.com>
2022-06-06 16:50:02 +0000

    2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, write_count;
+	
+	if(!filename)
+		return (-1);
+	
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0665);
+
+	if(text_content)
+	{
+		write_count = write(fd, text_content, strlen(text_content));
+		if(write_count < 0)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
+#define MAIN_H
 int _putchar(char ch);
 int create_file(const char *filename, char *text_content);
 #endif
